3939a22fa12f76c2940a4574ca970dd529ba0bca
lip <phillip@lipco.dev>
2021-03-18 15:18:18 -0700

    wrapping up for the evening, will complete login before lunch tomorrow

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Post } from '../models/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private post:Post) {
+    
+   }
 }
